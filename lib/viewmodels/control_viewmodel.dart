9cc954713ff53cd6602c45c1821a63f1d638b52c
BGM-109 <vividxxxxx@gmail.com>
2022-05-23 13:16:00 +0900

    feat: 바텀네비게이션 설정

--- lib/viewmodels/control_viewmodel.dart
+++ lib/viewmodels/control_viewmodel.dart
@@ -1,3 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_chat/views/account_view.dart';
+import 'package:flutter_firebase_chat/views/chat_view.dart';
+import 'package:flutter_firebase_chat/views/home_view.dart';
 
-class ControlViewModel extends ChangeNotifier {}
+class ControlViewModel extends ChangeNotifier {
+  int _currentIndex = 0;
+
+  int get currentIndex => _currentIndex;
+
+  void changeIndex(int index) {
+    _currentIndex = index;
+    notifyListeners();
+  }
+
+  Widget? currentView() {
+    switch (_currentIndex) {
+      case 0:
+        return const HomeView();
+      case 1:
+        return const ChatView();
+      case 2:
+        return const AccountView();
+      default:
+        return Container();
+    }
+  }
+}
--- lib/views/control_view.dart
+++ lib/views/control_view.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_chat/viewmodels/control_viewmodel.dart';
+import 'package:provider/provider.dart';
 
 class ControlView extends StatelessWidget {
   const ControlView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final vm = context.watch<ControlViewModel>();
+    return Scaffold(
+      body: vm.currentView(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: vm.currentIndex,
+        onTap: vm.changeIndex,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call),
+            label: '연락처',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: '채팅',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: '이름',
+          ),
+        ],
+      ),
+    );
   }
 }
